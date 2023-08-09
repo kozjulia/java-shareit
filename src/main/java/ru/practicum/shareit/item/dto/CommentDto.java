ba4d412086c70ae352e69c1f96dad6f634c3e643
Julia Kozlova <kozjuliajulia@yandex.ru>
2023-08-10 01:26:08 +0300

    feat: добавлены JsonTest на ДТО

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,8 +1,11 @@
 package ru.practicum.shareit.item.dto;
 
+import static ru.practicum.shareit.utils.Constants.PATTERN_FOR_DATETIME;
+
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Data
@@ -15,6 +18,7 @@ public class CommentDto {
 
     private String authorName;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = PATTERN_FOR_DATETIME)
     private LocalDateTime created;
 
 }
---
+++ src/test/java/ru/practicum/shareit/user/UserDtoTest.java
@@ -0,0 +1,37 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class UserDtoTest {
+    @Autowired
+    private JacksonTester<UserDto> json;
+
+    @Test
+    @DisplayName("получен ДТО пользователя, когда вызвана сериализация, " +
+            "то получен сериализованный пользователь")
+    void testUserDto() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId(1L);
+        userDto.setName("Name 1");
+        userDto.setEmail("mail1@mail.com");
+
+        JsonContent<UserDto> result = json.write(userDto);
+
+        assertThat(result).hasJsonPath("$.id");
+        assertThat(result).hasJsonPath("$.name");
+        assertThat(result).hasJsonPath("$.email");
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(userDto.getId().intValue());
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo(userDto.getName());
+        assertThat(result).extractingJsonPathStringValue("$.email").isEqualTo(userDto.getEmail());
+    }
+
+}
